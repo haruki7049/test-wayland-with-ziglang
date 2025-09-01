a9bdb9dccaf35e0910dec92574dc26b9c1e454c8
haruki7049 <tontonkirikiri@gmail.com>
2025-09-01 19:44:58 +0900

    build(repo): Init

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default-linux";
+    crane.url = "github:ipetkov/crane";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          buildInputs = [ ];
+          nativeBuildInputs = [
+            # Compiler
+            pkgs.zig_0_15
+
+            # Dependencies manager
+            pkgs.pkg-config
+
+            # LSP
+            pkgs.zls
+            pkgs.nil
+          ];
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Zig
+            programs.zig.enable = true;
+
+            # TOML
+            programs.taplo.enable = true;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+
+            # ShellScript
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inherit buildInputs nativeBuildInputs;
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
+        };
+    };
+}
---
+++ src/main.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+const test_wayland = @import("test_wayland_with_ziglang");
+
+pub fn main() !void {
+}
